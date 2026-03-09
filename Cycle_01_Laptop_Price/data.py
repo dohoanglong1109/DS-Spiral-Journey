5bc5cbaeb169ef09ea49346d29795b9b5614ba99
longdh <longdohoang@gmail.com>
2026-03-09 17:22:48 +0700

    Move 'Inches' to table Specs and add missing feature 'ScreenResolution' into Specs

--- Cycle_01_Laptop_Price/data.py
+++ Cycle_01_Laptop_Price/data.py
@@ -8,13 +8,13 @@ full_df = pd.read_csv(url)
 #%%
 # 2. "Phẫu thuật" dataset thành 2 phần để luyện SQL JOIN
 # Bảng 1: Thông tin thương hiệu và dòng máy
-df_laptops = full_df[['Company', 'TypeName', 'Inches']].copy()
+df_laptops = full_df[['Company', 'TypeName']].copy()
 df_laptops.index.name = 'laptop_id'
 df_laptops = df_laptops.reset_index()
 #%%
 # Bảng 2: Thông số kỹ thuật chi tiết và Giá
 # Lưu ý: Cột giá trong bộ này là 'Price', RAM là 'Ram', Trọng lượng là 'Weight'
-df_specs = full_df[['Cpu', 'Ram', 'Memory', 'Gpu', 'OpSys', 'Weight', 'Price']].copy()
+df_specs = full_df[['ScreenResolution', 'Inches', 'Cpu', 'Ram', 'Memory', 'Gpu', 'OpSys', 'Weight', 'Price']].copy()
 df_specs['laptop_id'] = df_laptops['laptop_id']
 #%%
 # 3. Đẩy vào SQLite ảo để luyện SQL
